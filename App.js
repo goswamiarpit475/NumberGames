64636acb74e58525d0c50b4af552e259915e4dd0
arpit <goswami.arpit475@gmail.com>
2023-01-20 15:41:43 +0530

    main screen added for application

--- App.js
+++ App.js
@@ -1,104 +1,61 @@
-//import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { TouchableOpacity, StatusBar, StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, FlatList, TouchableHighlight, ImageBackground, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LearnNumbers from './src/screens/numbers/LearnNumbers';
+import { Button } from 'react-native-paper';
 
-import * as Font from 'expo-font';
 
-import { Audio } from 'expo-av';
-import { LinearGradient } from 'expo-linear-gradient';
-export default function App() {
-  const [sound, setSound] = React.useState();
-  // const [fontsLoaded] = useFonts({
-  //   'Mouse-Font': require('./assets/fonts/Mousie.ttf'),
-  // });
-  const colorArray = [
-    ['#ff4da9', '#ffff66'], ['#ee9ca7', '#ffdde1'], ['#36d1dc', '#5b86e5'],
-    ['#1cd8d2', '#93edc7'],
-    ['#5c258d', '#4389a2'],
-    ['#134e5e', '#71b280'],
-    ['#2bc0e4', '#eaecc6'], ['#4776e6', '#8e54e9'], ['#ff8008', '#ffc837'], ['#1d976c', '#93f9b9'], ['#eb3349', '#f45c43'], ['#1fa2ff', '#12d8fa', '#a6ffcb'], ['#ff512f', '#f09819']
-  ];
-  const maxNumber = 10;
-  async function playSound(arg) {
-        await sound[arg-1].playAsync();
-        sound[arg-1].setPositionAsync(0);
-    
+
+
+
+function HomeScreen({navigation}) {
+
+  const ItemMenu=({item})=>{
+    return(
+      <ImageBackground style={{height:200,width:200,justifyContent:'center',alignItems:'center',alignSelf:'center',marginHorizontal:10}} imageStyle= 
+      {{opacity:0.8}} resizeMode="contain" source={require('./src/assets/images/cloud/cloud-red.png')}>
+      <TouchableOpacity onPress={()=>{navigation.navigate(item.navigationPath)}}>
+           <Text style={{fontSize: 25, color: 'white', fontWeight: 'bold'}}>{item.title}</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    )
   }
-  
-  React.useEffect(() => {
-    const loadFiles = async () => {
-      const { sound: sound1 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/1.mp3'));
-      const { sound: sound2 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/2.mp3'));
-      const { sound: sound3 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/3.mp3'));
-      const { sound: sound4 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/4.mp3'));
-      const { sound: sound5 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/5.mp3'));
-      const { sound: sound6 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/6.mp3'));
-      const { sound: sound7 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/7.mp3'));
-      const { sound: sound8 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/8.mp3'));
-      const { sound: sound9 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/9.mp3'));
-      const { sound: sound10 } = await Audio.Sound.createAsync(require('./assets/voices/number_voice/10.mp3'));
-      //setSound({ s1: sound1, s2: sound2, s3: sound3, s4: sound4, s5: sound5, s6: sound6, s7: sound7, s8: sound8, s9: sound9, s10: sound10 });
-    setSound([sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9,sound10 ]);
-    }
-    loadFiles();
 
-  }, []);
+
 
   return (
     <>
-      <View
-        style={[
-          styles.container,
-          {
-            // Try setting `flexDirection` to `"row"`.
-            flexDirection: 'column',
-          },
-        ]}>
-
-        <ImageBackground resizeMode="cover" source={require('./assets/images/gif/108109-moving-grass.gif')} 
-        style={{ flex: 4, flexDirection: 'row', backgroundColor: '#ccffff', flexWrap: 'wrap', justifyContent: 'center',alignContent:'center' }}>
-
-          {[...Array(maxNumber)].map((x, i) =>
-            <TouchableOpacity onPress={() => playSound(i + 1)} style={styles.myCard}>
-              <LinearGradient colors={colorArray[i%12]} style={styles.CircleShape}>
-                <Text style={styles.numberText}>{i + 1}
-                </Text>
-              </LinearGradient>
-            </TouchableOpacity>
-          )}
-
-
-        </ImageBackground>
-
-        <StatusBar style='auto' />
-      </View>
+  
+  <ImageBackground style={{flex:1}} resizeMode="cover" source={require('./src/assets/images/gif/under-water.gif')}>
+    
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <FlatList horizontal 
+        data={[{title: 'Learn Number', key: 'item1',navigationPath:'LearnNumbers'}]}
+        renderItem={({item}) => <ItemMenu item={item} />}
+      />
+    </View>
+    <StatusBar style='auto' />
+    </ImageBackground>
     </>
   );
-};
+}
+{/* <TouchableOpacity onPress={()=>{navigation.navigate('LearnNumbers')}} style={{height:200,width:150,borderRadius:50,backgroundColor:'#EAA338',marginStart:10,justifyContent:'center',alignItems:'center',                    
+                    }}>
+         <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>Learn Numbers</Text>
+      </TouchableOpacity> */}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //padding: 20,
-  },
-  myCard: { margin: 20, alignItems: 'center', justifyContent: 'center' },
-  numberText: {
-    fontSize: 50, color: 'white', fontWeight: '900'
-  },
-  //grad:{backgroundImage: linearGradient('red', 'yellow')},
-  tinyLogo: {
-    width: 100,
-    height: 100,
-  },
-  CircleShape: {
-    width: 80,
-    height: 80,
-    borderRadius: 80 / 2,
-    backgroundColor: '#FF9800',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 5
-  },
-});
+const Stack = createNativeStackNavigator();
 
-//export default Flex;
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="LearnNumbers" component={LearnNumbers} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/screens/numbers/LearnNumbers.js
@@ -0,0 +1,125 @@
+//import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { TouchableOpacity,StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+import * as Font from 'expo-font';
+
+import { Audio } from 'expo-av';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button } from 'react-native-paper';
+export default function LearnNumbers({navigation}) {
+  const [sound, setSound] = React.useState();
+  // const [fontsLoaded] = useFonts({
+  //   'Mouse-Font': require('./assets/fonts/Mousie.ttf'),
+  // });
+  const colorArray = [
+    ['#ff4da9', '#ffff66'], ['#ee9ca7', '#ffdde1'], ['#36d1dc', '#5b86e5'],
+    ['#1cd8d2', '#93edc7'],
+    ['#5c258d', '#4389a2'],
+    ['#134e5e', '#71b280'],
+    ['#2bc0e4', '#eaecc6'], ['#4776e6', '#8e54e9'], ['#ff8008', '#ffc837'], ['#1d976c', '#93f9b9'], ['#eb3349', '#f45c43'], ['#1fa2ff', '#12d8fa', '#a6ffcb'], ['#ff512f', '#f09819']
+  ];
+  const maxNumber = 10;
+  async function playSound(arg) {
+        await sound[arg-1].playAsync();
+        sound[arg-1].setPositionAsync(0);
+  }
+  
+  React.useEffect(() => {
+    const loadFiles = async () => {
+      console.log('loading Sound');
+      const { sound: sound1 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/1.mp3'));
+      const { sound: sound2 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/2.mp3'));
+      const { sound: sound3 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/3.mp3'));
+      const { sound: sound4 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/4.mp3'));
+      const { sound: sound5 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/5.mp3'));
+      const { sound: sound6 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/6.mp3'));
+      const { sound: sound7 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/7.mp3'));
+      const { sound: sound8 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/8.mp3'));
+      const { sound: sound9 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/9.mp3'));
+      const { sound: sound10 } = await Audio.Sound.createAsync(require('../../assets/voices/number_voice/10.mp3'));
+      //setSound({ s1: sound1, s2: sound2, s3: sound3, s4: sound4, s5: sound5, s6: sound6, s7: sound7, s8: sound8, s9: sound9, s10: sound10 });
+    setSound([sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9,sound10 ]);
+    }
+    loadFiles();
+  }, []);
+
+  React.useEffect(() => {
+    return  sound?() => {
+          console.log('Unloading Sound');
+          sound[0].unloadAsync();
+       sound[1].unloadAsync();
+       sound[2].unloadAsync();
+       sound[3].unloadAsync();
+       sound[4].unloadAsync();
+       sound[5].unloadAsync();
+       sound[6].unloadAsync();
+       sound[7].unloadAsync();
+       sound[8].unloadAsync();
+       sound[9].unloadAsync();
+        }:undefined
+  }, [sound]);
+
+  return (
+    <>
+      <View
+        style={[
+          styles.container,
+          {
+            // Try setting `flexDirection` to `"row"`.
+            flexDirection: 'column',
+          },
+        ]}>
+
+        <ImageBackground style={{flex:1}} resizeMode="cover" source={require('../../assets/images/gif/108109-moving-grass.gif')}>
+        <Button onPress={()=>navigation.goBack()} style={{alignSelf:"flex-start"}}
+        icon={({ size, color }) => (
+          <Image
+            source={require('../../assets/images/back-icon.png')}
+            style={{ width: 50, height: 50,marginTop:15 }}
+          />
+        )}
+        />
+        <View style={{ flex: 4, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center',alignContent:'center' }}>
+          {[...Array(maxNumber)].map((x, i) =>
+            <TouchableOpacity onPress={() => playSound(i + 1)} style={styles.myCard}>
+              <LinearGradient colors={colorArray[i%12]} style={styles.CircleShape}>
+                <Text style={styles.numberText}>{i + 1}
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          )}
+        </View>
+
+        </ImageBackground>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //padding: 20,
+  },
+  myCard: { margin: 20, alignItems: 'center', justifyContent: 'center' },
+  numberText: {
+    fontSize: 50, color: 'white', fontWeight: '900'
+  },
+  //grad:{backgroundImage: linearGradient('red', 'yellow')},
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+  CircleShape: {
+    width: 80,
+    height: 80,
+    borderRadius: 80 / 2,
+    backgroundColor: '#FF9800',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 5
+  },
+});
+
+//export default Flex;
